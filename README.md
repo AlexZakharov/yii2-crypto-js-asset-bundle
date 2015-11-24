6f539e87bf045e5447735abf1d3dc22eb5a27d93
Cyril Turkevich <turkevich.kfos@gmail.com>
2015-11-24 02:25:19 +0200

    add crypto js asset bundle

---
+++ README.md
@@ -0,0 +1,18 @@
+# Yii2 CryptoJS Asset Bundle
+## Installation
+
+Installation with Composer
+
+Either run
+~~~
+    php composer.phar require kfosoft/yii2-crypto-js-asset-bundle:"*"
+~~~
+or add in composer.json
+~~~
+    "require": {
+        ...
+        "kfosoft/yii2-crypto-js-asset-bundle":"*"
+    }
+~~~
+
+Enjoy, guys!
---
+++ asset/CryptoJSBundle.php
@@ -0,0 +1,22 @@
+<?php
+namespace kfosoft\yii2\editable;
+
+use yii\web\AssetBundle;
+
+/**
+ * Crypto js asset bundle.
+ * @see http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js
+ * @package app\services
+ * @version 1.0
+ * @copyright (c) 2014-2015 KFOSoftware Team <kfosoftware@gmail.com>
+ */
+class CryptoJSBundle extends AssetBundle
+{
+    /** @var string path to source folder */
+    public $sourcePath = '@vendor/kfosoft/yii2-crypto-js-asset-bundle/asset/assets';
+
+    /** @var array js files. */
+    public $js = [
+        'js/crypto-js.js',
+    ];
+} 
